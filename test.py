e68532e5f61b98c5b8305b739f44119fa13b4906
Arfath Khan <arafathkhan8315@gmail.com>
2026-03-12 22:25:08 +0530

    new python file

---
+++ test.py
@@ -0,0 +1,8 @@
+# Print a simple message to the console
+print("Hello, World!")
+
+# Get the user's name as input and store it in a variable
+name = input("What is your name?\n") 
+
+# Print a greeting using the input name with an f-string (formatted string literal)
+print(f"Hi, {name}.")
